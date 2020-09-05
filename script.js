7d8dc6a5cb9cf8143bcd71dcc1b65d4543d0712f
saaarvesh <47692937+saaarvesh@users.noreply.github.com>
2020-09-05 14:24:43 +0530

    final

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    if (!isChrome) {
-      $('#iframeAudio').remove()
-    }
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3'); 
+audio.loop=true;
+ audio.play();
